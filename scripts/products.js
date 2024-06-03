5be53c6de53337836d4a57440207680641004d31
Thauan Lim'as <116463820+ThauanLim4@users.noreply.github.com>
2024-06-03 19:15:37 -0300

    att 002

--- scripts/products.js
+++ scripts/products.js
@@ -1,22 +1,270 @@
-const placasDeVideo = [
-    {
-        id: 1,
-        name: 'Placa de Video Mancer RX 5500 XT Streaky, 8GB, GDDR6, 128-bit, MCR-RX5500XT',
-        price: 1038.90,
-        img: '../components/imgProducts/pdv/rxd5500xt.png'
-    },
 
-    {
-        id: 2,
-        name: 'MSI GeForce GTX 1650 128-Bit HDMI/DP/DVI 4GB GDRR6',
-        price: 939.00,
-        imgProduto: '../components/imgProducts/pdv/gtx1650.png'
-    },
+const hardwares = [
+    [
+        {
+            id: 1,
+            name: 'Placa de Video Mancer RX 5500 XT Streaky, 8GB, GDDR6, 128-bit, MCR-RX5500XT',
+            price: 1038.90,
+            img: '../components/imgProducts/pdv/rx5500xt.png'
+        },
 
-    {
-        id: 3,
-        name: 'Placa de Vídeo Gigabyte - GeForce GTX 1660, 6GB GDDR5',
-        price: 1772.70,
-        img: '../components/imgProducts/pdv/gtx1660.png'
-    }
+        {
+            id: 2,
+            name: 'MSI GeForce GTX 1650 128-Bit HDMI/DP/DVI 4GB GDRR6',
+            price: 939.00,
+            img: '../components/imgProducts/pdv/gtx1650.png'
+        },
+
+        {
+            id: 3,
+            name: 'Placa de Vídeo Gigabyte - GeForce GTX 1660, 6GB GDDR5',
+            price: 1772.70,
+            img: '../components/imgProducts/pdv/gtx1660.png'
+        }
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Processador Intel Core I5-12400F 2.5GHZ (Turbo 4.4GHZ) Cache 18MB 6 Núcleos 12 Threads 12ª Ger LGA 1700',
+            price: 919.90,
+            img: '../components/imgProducts/cpu/i5-12400f.png'
+        },
+    
+        {
+            id: 2,
+            name: 'Processador AMD Ryzen 5 5600G, 3.9GHz (4.4GHz Max Turbo), AM4, Vídeo Integrado, 6 Núcleos',
+            price: 833.25,
+            img: '../components/imgProducts/cpu/r5-5600g.png'
+        },
+    
+        {
+            id: 3,
+            name: 'PROCESSADOR AMD RYZEN 7 5700X 3.4GHz (TURBO 4.6GHz) AM4, Cerâmica cinza ',
+            price: 1129.90,
+            img: '../components/imgProducts/cpu/r7-5700x.png'
+        },
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Placa Mãe Gigabyte A520M S2H DDR4, AMD, AM4',
+            price: 579.90,
+            img: '../components/imgProducts/mboard/a520h-s2.png'
+        },
+    
+        {
+            id: 2,
+            name: 'Placa Mãe Gigabyte B450M DS3H V2',
+            price: 588.00,
+            img: '../components/imgProducts/mboard/B450M-DS3H-V2.png'
+        },
+    
+        {
+            id: 3,
+            name: 'Placa Mãe Gigabyte B550M AORUS Elite, Chipset B550, AMD AM4, mATX, DDR4',
+            price: 729.98,
+            img: '../components/imgProducts/mboard/b550m.png'
+        },
+    
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'HD Seagate BarraCuda, 1TB, 3.5, SATA',
+            price: 317.90,
+            img: '../components/imgProducts/armaz/hd-1TB.png'
+        },
+        
+        {
+            id: 2,
+            name: 'SSD Kingston NV2 500GB NVMe M.2 2280 ',
+            price: 312.00,
+            img: '../components/imgProducts/armaz/nvme-500gb.png'
+        },
+        
+        {
+            id: 3,
+            name: 'HD SSD Kingston SA400S37 480GB ',
+            price: 259.90,
+            img: '../components/imgProducts/armaz/ssd.png'
+        },
+        
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Gabinete Gamer Pichau HX300M Glass, Mid-tower, Lateral de Vidro, Branco',
+            price: 258.90,
+            img: '../components/imgProducts/gabinete/hx300.png'
+        },
+        
+        {
+            id: 2,
+            name: 'GABINETE MICRO ATX SEM FONTE F02 PRETO BRX ',
+            price: 100.69,
+            img: '../components/imgProducts/gabinete/brx.png'
+        },
+        
+        {
+            id: 3,
+            name: 'Gabinete Redragon Wideload Pro CA-604B-PRO',
+            price: 389.98,
+            img: '../components/imgProducts/gabinete/CA-604B-PRO.png'
+        },
+        
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Corsair FONTE ATX 750W - CV750-80 PLUS BRONZE - COM CABO DE FORÇA',
+            price: 789.90,
+            img: '../components/imgProducts/fontes/cv750w.png'
+        },
+        
+        {
+            id: 2,
+            name: 'Fonte de Alimentação Redragon 500W 80 Plus Bronze PFC Ativo S/Cabo',
+            price: 314.39,
+            img: '../components/imgProducts/fontes/reddragon-500w.png'
+        },
+        
+        {
+            id: 3,
+            name: 'FONTE ATX 650W - CV650-80 PLUS BRONZE - COM CABO DE FORCA',
+            price: 449.00,
+            img: '../components/imgProducts/fontes/cv650w.png'
+        },
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Monitor AOC 23,8',
+            price: 539.10,
+            img: '../components/imgProducts/monitores/aoc23.png'
+        },
+        
+        {
+            id: 2,
+            name: 'AOC Hero 27" - Monitor Gamer, 144Hz IPS 1ms G-Sync Compatible 27G2, Preto ',
+            price: 1299.00,
+            img: '../components/imgProducts/monitores/aoc-hero27.png'
+        },
+        
+        {
+            id: 3,
+            name: 'Samsung T350 - Monitor Gamer, 24", FHD, 75Hz, HDMI, VGA, Freesync, Preto ',
+            price: 609.00,
+            img: '../components/imgProducts/monitores/t350.png'
+        },
+        
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Caixa de Som Gamer Redragon Anvil RGB Branco - GS560W',
+            price: 339.90,
+            img: '../components/imgProducts/som/gs560w.png'
+        },
+        
+        {
+            id: 2,
+            name: 'CX DE SOM SOUNDBAR GAMER REDRAGON ADIEMUS - GS560 ',
+            price: 369.00,
+            img: '../components/imgProducts/som/gs560.png'
+        },
+        
+        {
+            id: 3,
+            name: 'C3Tech Caixa de som SP-301BK Preto - Sistema de audio 2.0 P2 Com Blindagem Eletromagnética alimentacao via porta USB ',
+            price: 24.90,
+            img: '../components/imgProducts/som/g3tech.png'
+        },
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Redragon TECLADO MECANICO GAMER FIZZ RGB PRETO SWITCH MARROM',
+            price: 208.44,
+            img: '../components/imgProducts/perifericos/redDragon-fizz.png'
+        },
+        
+        {
+            id: 2,
+            name: 'Mouse Gamer Logitech G203 LIGHTSYNC RGB, Efeito de Ondas de Cores, 6 Botões Programáveis e Até 8.000 DPI - Preto ',
+            price: 149.88,
+            img: '../components/imgProducts/perifericos/Logitech-G203.png'
+        },
+        
+        {
+            id: 3,
+            name: 'Headphone Fone de Ouvido Havit HV-H2002d, Gamer, com Microfone, Falante 53mm, Plug 3.5mm',
+            price: 175.98,
+            img: '../components/imgProducts/perifericos/havit-hv-h2002d.png'
+        },
+        
+    ]
+
 ]
+
+let [placasDeVideo, cpu, placaMae, armazenamento, gabinete, fontes, perifericos, monitores, som] = hardwares
+
+// const placasDeVideo = [
+    
+// ]
+
+// const cpu = [
+
+// ]
+
+// const placaMae = [
+// ]
+
+// const perifericos = [
+// ]
+
+// const armazenamento = [
+// ]
+
+// const gabinete = [
+// ]
+
+// const fontes = [
+
+// ]
+
+// const monitores = [
+// ]
+
+// const som = [
+
+// ]
+
+
+// // {
+// //     id: 1,
+// //     name: '',
+// //     price: ,
+// //     img: '../components/imgProducts/'
+// // },
+
+// // {
+// //     id: 2,
+// //     name: '',
+// //     price: ,
+// //     img: '../components/imgProducts/'
+// // },
+
+// // {
+// //     id: 3,
+// //     name: '',
+// //     price: ,
+// //     img: '../components/imgProducts/'
+// // },
--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,154 @@
 const sectionItens = document.getElementById('sectionItens')
+const categories = document.querySelector('.categories')
+const productsOfCategory = document.querySelector('.productsOfCategory')
+const sectionProductsOfCategory = document.querySelector('.sectionProductsOfCategory')
+const home = document.getElementById('home')
+const recommendedProducts = document.getElementById('recommendedProducts')
+const footer = document.getElementById('footer')
 
-placasDeVideo.map((item, index) =>{
-    let itemCategoria = document.querySelector('.model .itemCategoria').cloneNode(true)
+let itensOfCategory = document.querySelector('.itensOfCategory')
+let linkItemCategoria = document.querySelectorAll('.linkItemCategoria')
+let backToCategories = document.querySelector('.backToCategories')
+let headerListItem = document.querySelectorAll('.headerListItem')
+let headerList = document.querySelector('#headerList')
 
-    itemCategoria.setAttribute('data-index', index)
-    itemCategoria.querySelector('.imgItemCategoria').src = item.id
-    itemCategoria.querySelector('.titleItemCategoria').innerHTML = item.name
-    itemCategoria.querySelector('.priceItem').innerHTML = item.price
 
-    sectionItens.prepend(itemCategoria)
+let observer = new IntersectionObserver((entries, observer) =>{
+    entries.forEach(entry =>{
+        if(entry.isIntersecting){
+            console.log('o elemento '+entries+' está na tela')
+        }
+    })
+}, {threshold: 0.1})
+
+headerListItem.forEach((item, index) =>{
+    item.addEventListener('',(i) =>{
+        if(home){
+            observer.observe(home)
+            console.log(i)
+        } 
+        if (categories){
+            observer.observe(categories)        
+        } 
+        if (recommendedProducts){
+            observer.observe(recommendedProducts)
+            
+        } 
+        if (footer) {
+            observer.observe(footer)
+        }
+    
+    })
+    
+    removeInView()
 })
+
+
+
+
+linkItemCategoria.forEach((item, index) => {
+    item.addEventListener('click', (i) => {
+        i.preventDefault()
+        let valueAtribute = item.getAttribute('data-key')
+        console.log(valueAtribute)
+
+        let returnValue = hardwares.filter((element) => element.indexOf(valueAtribute))
+        let selectedCategory = hardwares[valueAtribute]
+
+        categories.classList.add('hidden')
+        setTimeout(() => {
+            categories.style.display = 'none'
+            productsOfCategory.style.transform = 'translateX(0%)'
+            sectionProductsOfCategory.style.transform = 'translateX(0%)'
+        }, 1000)
+        sectionProductsOfCategory.style.display = 'flex'
+        productsOfCategory.style.display = 'grid'
+
+        selectedCategory.map((product, index) => {
+            let indentifierCategory = document.querySelector('.indentifierCategory')
+            let itemProduct = document.querySelector('.model .itemProductCategory').cloneNode(true)
+
+            itemProduct.setAttribute('data-index', index)
+            itemProduct.querySelector('.imgCategory').setAttribute('src', `${product.img}`)
+            itemProduct.querySelector('.titleCategory').innerHTML = product.name
+            itemProduct.querySelector('.priceCategory').innerHTML = `R$ ${product.price.toFixed(2)}`
+
+            productsOfCategory.append(itemProduct)
+
+            switch (valueAtribute) {
+                case '0':
+                    indentifierCategory.innerHTML = 'placa de vídeo'
+                    console.log('ok')
+                    break;
+
+                case '1':
+                    indentifierCategory.innerHTML = 'processador'
+                    console.log('ok')
+                    break;
+
+                case '2':
+                    indentifierCategory.innerHTML = 'placa mãe'
+                    console.log('ok')
+                    break;
+
+                case '3':
+                    indentifierCategory.innerHTML = 'armazenamento'
+                    console.log('ok')
+                    break;
+
+                case '4':
+                    indentifierCategory.innerHTML = 'gabinete'
+                    console.log('ok')
+                    break;
+
+                case '5':
+                    indentifierCategory.innerHTML = 'fonte de alimentação'
+                    console.log('ok')
+                    break;
+
+                case '6':
+                    indentifierCategory.innerHTML = 'monitor'
+                    console.log('ok')
+                    break;
+
+                case '7':
+                    indentifierCategory.innerHTML = 'som'
+                    console.log('ok')
+                    break;
+
+                case '8':
+                    indentifierCategory.innerHTML = 'perífericos'
+                    console.log('ok')
+                    break;
+            }
+
+            backToCategories.addEventListener('click', returnToCategories)
+
+
+        })
+
+
+        console.log(returnValue)
+    })
+})
+
+function returnToCategories() {
+    categories.classList.remove('hidden')
+    productsOfCategory.style.transform = 'translateX(-200%)'
+    sectionProductsOfCategory.style.transform = 'translateX(-200%)'
+
+    setTimeout(() => {
+        categories.style.display = 'flex'
+        sectionProductsOfCategory.style.display = 'none'
+        productsOfCategory.style.display = 'none'
+        productsOfCategory.innerHTML = ''
+    }, 1000)
+}
+
+function removeInView(){
+    headerListItem.forEach(i =>{
+        i.classList.remove('inView')
+    })
+}
+
+
